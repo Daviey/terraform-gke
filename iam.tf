3b1419aef26304ef53c91c35c8af4bb8034a5131
ssMMiles <github@milesmoonlove.com>
2020-12-26 16:58:43 +0000

    Revert "Merge pull request #6 from TeamS0pht/add-miles"
    This reverts commit 331a7fc5e1c48866c87425e6eb313e7b6be310c6, reversing
    changes made to 873911fa2d691b8f556c3047df57324a1b6c1e00.

--- iam.tf
+++
@@ -1,9 +0,0 @@
-resource "google_project_iam_binding" "project" {
-  project = var.project_id
-  role    = "roles/editor"
-
-  members = [
-    "user:davieywalker@gmail.com",
-    "user:admin@milesmoonlove.com",
-  ]
-}

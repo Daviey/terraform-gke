ee5ddea5ca33728ad9f1042417b78e1c0907509d
Dave Walker (Daviey) <email@daviey.com>
2020-12-26 11:56:03 +0000

    Added GKE cluster
    Signed-off-by: Dave Walker (Daviey) <email@daviey.com>

---
+++ gke.tf
@@ -0,0 +1,88 @@
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 3
+  description = "number of gke nodes"
+}
+
+variable "machine_type" {
+  description = "machine_type"
+}
+
+variable "preemptible" {
+  default     = true
+  description = "preemptible"
+}
+
+variable "location" {
+  description = "location"
+}
+
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.location
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+
+  master_auth {
+    username = var.gke_username
+    password = var.gke_password
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name     = "${google_container_cluster.primary.name}-node-pool"
+  location = var.location
+  cluster  = google_container_cluster.primary.name
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = var.gke_num_nodes
+  }
+
+  management {
+    auto_repair = true
+  }
+
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    preemptible  = var.preemptible
+    machine_type = var.machine_type
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
+output "kubernetes_cluster_name" {
+  value       = google_container_cluster.primary.name
+  description = "GKE Cluster Name"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+project_id    = "bugbounty-299720"
+region        = "europe-west2"
+location      = "europe-west2-a"
+machine_type  = "g1-small"
+gke_num_nodes = 1
